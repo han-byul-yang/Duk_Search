2641eafd97061f637897333a0a891ba9cf8a2099
Han Byul <yahb9@naver.com>
2022-06-18 21:26:00 +0900

    Config: Inital setting

---
+++ src/routes/index.tsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from 'react-router-dom'
+import { RecoilRoot } from 'recoil'
+
+import Main from 'components/Main'
+import Search from 'components/Search'
+
+import styles from './routes.module.scss'
+
+const App = () => {
+  return (
+    <div className={styles.page}>
+      <RecoilRoot>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='search' element={<Search />} />
+          <Route path='*' element={<div>404</div>} />
+        </Routes>
+      </RecoilRoot>
+    </div>
+  )
+}
+
+export default App
