7f40abf050f18a982258bcb88955763f3c0a2416
Han Byul <yahb9@naver.com>
2022-06-22 19:52:39 +0900

    Feat: EventLister 이용하여 사이즈 조정 시 navBar 변동

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
 import { ChartIcon, HamburgerIcon, HomeIcon, LogoIcon } from 'assets/svgs'
@@ -5,35 +6,51 @@ import { ChartIcon, HamburgerIcon, HomeIcon, LogoIcon } from 'assets/svgs'
 import styles from './navBar.module.scss'
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => setIsOpen(!(window.innerWidth < 1050))
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
     <nav className={styles.navigation}>
-      <Link to='/'>
-        <LogoIcon className={styles.logo} />
-      </Link>
-      <ul className={styles.navigationList}>
-        <NavLink
-          to='/search'
-          className={({ isActive }) =>
-            `${styles.navLink} + ${isActive ? styles.activatedLink : styles.nonActivatedLink}`
-          }
-        >
-          <li>
-            <HomeIcon className={styles.navIcon} />
-            SEARCH
-          </li>
-        </NavLink>
-        <NavLink
-          to='/chart'
-          className={({ isActive }) =>
-            `${styles.navLink} + ${isActive ? styles.activatedLink : styles.nonActivatedLink}`
-          }
-        >
-          <li>
-            <ChartIcon className={styles.navIcon} />
-            CHART
-          </li>
-        </NavLink>
-      </ul>
+      {isOpen ? (
+        <>
+          <Link to='/'>
+            <LogoIcon className={styles.logo} />
+          </Link>
+          <ul className={styles.navigationList}>
+            <NavLink
+              to='/search'
+              className={({ isActive }) =>
+                `${styles.navLink} + ${isActive ? styles.activatedLink : styles.nonActivatedLink}`
+              }
+            >
+              <li>
+                <HomeIcon className={styles.navIcon} />
+                SEARCH
+              </li>
+            </NavLink>
+            <NavLink
+              to='/chart'
+              className={({ isActive }) =>
+                `${styles.navLink} + ${isActive ? styles.activatedLink : styles.nonActivatedLink}`
+              }
+            >
+              <li>
+                <ChartIcon className={styles.navIcon} />
+                CHART
+              </li>
+            </NavLink>
+          </ul>
+        </>
+      ) : (
+        <HamburgerIcon />
+      )}
     </nav>
   )
 }
