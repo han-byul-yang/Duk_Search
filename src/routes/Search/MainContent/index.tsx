9a3bc1663e0edf7015f0523a4ee54add540b15b1
Han Byul <yahb9@naver.com>
2022-06-23 02:22:59 +0900

    Feat: useParams setting in MainContent

--- src/routes/Search/MainContent/index.tsx
+++ src/routes/Search/MainContent/index.tsx
@@ -1,5 +1,9 @@
+import { useParams } from 'react-router-dom'
+
 const MainContent = () => {
-  return <div>maincontent</div>
+  const params = useParams()
+
+  return <div>{params?.category}</div>
 }
 
 export default MainContent
