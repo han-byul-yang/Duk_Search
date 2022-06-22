6d88564432472654c310d7066b35a84b3f337f09
Han Byul <yahb9@naver.com>
2022-06-22 18:58:27 +0900

    Design: navigation 스타일 수정

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,19 +1,38 @@
+import { Link, NavLink } from 'react-router-dom'
+
 import { ChartIcon, HamburgerIcon, HomeIcon, LogoIcon } from 'assets/svgs'
+
 import styles from './navBar.module.scss'
 
 const NavBar = () => {
   return (
     <nav className={styles.navigation}>
-      <LogoIcon className={styles.logo} />
+      <Link to='/'>
+        <LogoIcon className={styles.logo} />
+      </Link>
       <ul className={styles.navigationList}>
-        <li>
-          <HomeIcon />
-          SEARCH
-        </li>
-        <li>
-          <ChartIcon />
-          CHART
-        </li>
+        <NavLink
+          to='/search'
+          className={({ isActive }) =>
+            `${styles.navLink} + ${isActive ? styles.activatedLink : styles.nonActivatedLink}`
+          }
+        >
+          <li>
+            <HomeIcon className={styles.navIcon} />
+            SEARCH
+          </li>
+        </NavLink>
+        <NavLink
+          to='/chart'
+          className={({ isActive }) =>
+            `${styles.navLink} + ${isActive ? styles.activatedLink : styles.nonActivatedLink}`
+          }
+        >
+          <li>
+            <ChartIcon className={styles.navIcon} />
+            CHART
+          </li>
+        </NavLink>
       </ul>
     </nav>
   )
