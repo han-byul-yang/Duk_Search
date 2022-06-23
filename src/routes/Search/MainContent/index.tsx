cd46b1ecee31676f576be2aeded9f5c5882a8174
Han Byul <yahb9@naver.com>
2022-06-24 05:24:52 +0900

    Feat: Make modal work

--- src/routes/Search/MainContent/index.tsx
+++ src/routes/Search/MainContent/index.tsx
@@ -7,6 +7,8 @@ import { getTwitterData } from 'services/getData'
 import twitterData from '../../../data/twitterDatas.json'
 import { searchKeyAtom } from 'store/atoms'
 import { ITwitterData } from 'types/types'
+import ModalPortal from '../Modal/ModalPortal'
+import Modal from '../Modal'
 
 import styles from './mainContent.module.scss'
 
@@ -16,6 +18,8 @@ interface ICategory {
 
 const MainContent = ({ category }: ICategory) => {
   const [twitterDataList, setTwitterDataList] = useState<ITwitterData[]>()
+  const [clickedImges, setClickedImges] = useState<string[]>()
+  const [openModal, setOpenModal] = useState(false)
   const searchKey = useRecoilValue(searchKeyAtom)
   const params = useParams()
 
@@ -29,37 +33,49 @@ const MainContent = ({ category }: ICategory) => {
   //   return <div className='isLoading'>로딩 중...</div>
   // }
 
+  const handleImgesClick = (img: string[]) => {
+    setClickedImges(img)
+    setOpenModal(true)
+  }
+
   return (
-    <div>
-      {/* eslint-disable-next-line no-nested-ternary */}
-      {category ? (
-        twitterData.map((data) => {
-          const { id, nickName, date, img, text } = data
-          const dataKey = `data-${id}`
-          return (
-            <div key={dataKey} className={styles.container}>
-              <div className={styles.nickName}>{nickName}</div>
-              <div className={styles.date}>{date}</div>
-              <div className={styles.text}>{text}</div>
-              {img.length !== 0 && (
-                <div className={styles.imgContainer}>
-                  {img.map((imgUrl, i) => {
-                    const imgKey = `imgUrl-${i}`
-                    return <img key={imgKey} className={styles.img} src={`${imgUrl}`} alt='게시글 이미지' />
-                  })}
-                </div>
-              )}
-            </div>
-          )
-        })
-      ) : searchKey ? (
-        <div>
-          {params?.category} 카테고리에서 찾은 {searchKey}
-        </div>
-      ) : (
-        <div>검색어를 입력하세요</div>
+    <>
+      <main>
+        {/* eslint-disable-next-line no-nested-ternary */}
+        {category ? (
+          twitterData.map((data) => {
+            const { id, nickName, date, img, text } = data
+            const dataKey = `data-${id}`
+            return (
+              <div key={dataKey} className={styles.container}>
+                <div className={styles.nickName}>{nickName}</div>
+                <div className={styles.date}>{date}</div>
+                <div className={styles.text}>{text}</div>
+                {img.length !== 0 && (
+                  <button className={styles.imgContainer} type='button' onClick={() => handleImgesClick(img)}>
+                    {img.map((imgUrl, i) => {
+                      const imgKey = `imgUrl-${i}`
+                      return <img key={imgKey} className={styles.img} src={`${imgUrl}`} alt='게시글 이미지' />
+                    })}
+                  </button>
+                )}
+              </div>
+            )
+          })
+        ) : searchKey ? (
+          <div>
+            {params?.category} 카테고리에서 찾은 {searchKey}
+          </div>
+        ) : (
+          <div>검색어를 입력하세요</div>
+        )}
+      </main>
+      {openModal && (
+        <ModalPortal>
+          <Modal clickedImges={clickedImges} setOpenModal={setOpenModal} />
+        </ModalPortal>
       )}
-    </div>
+    </>
   )
 }
 
@@ -68,3 +84,7 @@ export default MainContent
 // 동영상도 볼 수 있게 해야함
 // 날짜 24시간 이내면 몇 분 전~ 식으로 표시해주고 그렇지 않으면 그냥 날짜 표시
 // 사진 누르면 크게 띄워주기, 여러 개면 옆으로 넘기는 기능 추가
+// submit하면 전체 카테고리 보여주는 페이지로 넘어가기
+// 사진 모달 띄워주기
+// 캐로셀 구현
+// 퍼지 문자열 검색 구현
--- src/routes/Search/Modal/index.tsx
+++ src/routes/Search/Modal/index.tsx
@@ -1,9 +1,30 @@
+import { Dispatch, SetStateAction } from 'react'
+
+import { XIcon } from 'assets/svgs'
+
 import styles from './modal.module.scss'
 
-const Modal = () => {
+interface IModalProps {
+  clickedImges?: string[]
+  setOpenModal: Dispatch<SetStateAction<boolean>>
+}
+
+const Modal = ({ clickedImges, setOpenModal }: IModalProps) => {
+  const handleCloseModalClick = () => {
+    setOpenModal(false)
+  }
+
   return (
     <div className={styles.background}>
-      <div className={styles.container} />
+      <button type='button' onClick={handleCloseModalClick}>
+        <XIcon className={styles.xIcon} />
+      </button>
+      <div className={styles.container}>
+        {clickedImges?.map((img, i) => {
+          const imgKey = `img-${i}`
+          return <img key={imgKey} src={img} alt='모달이미지' />
+        })}
+      </div>
     </div>
   )
 }
