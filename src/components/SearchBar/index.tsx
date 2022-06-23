906d9201043fabbf6293eb13c2d9bdfc6eee301c
Han Byul <yahb9@naver.com>
2022-06-24 01:45:46 +0900

    Feat: Show currentKeyList with searhed key history

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,7 +1,7 @@
 import { ChangeEvent, FormEvent, useState } from 'react'
-import { useSetRecoilState } from 'recoil'
+import { useRecoilState, useSetRecoilState } from 'recoil'
 
-import { searchKeyAtom } from 'store/atoms'
+import { searchKeyAtom, searchKeyListAtom } from 'store/atoms'
 import { SearchIcon } from 'assets/svgs'
 
 import styles from './searchBar.module.scss'
@@ -9,6 +9,7 @@ import styles from './searchBar.module.scss'
 const SearchBar = () => {
   const [valueInput, setValueInput] = useState('')
   const setSearchKey = useSetRecoilState(searchKeyAtom)
+  const [searchKeyList, setSearchKeyList] = useRecoilState(searchKeyListAtom)
 
   const handleInputValueChange = (e: ChangeEvent<HTMLInputElement>) => {
     setValueInput(e.currentTarget.value)
@@ -17,14 +18,28 @@ const SearchBar = () => {
   const handleInputValueSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     setSearchKey(valueInput)
+    setSearchKeyList((prevList) => {
+      if (prevList?.indexOf(valueInput) === -1) {
+        return [...prevList, valueInput]
+      }
+      return prevList
+    })
   }
 
   return (
     <form className={styles.inputForm} onSubmit={handleInputValueSubmit}>
       <SearchIcon className={styles.searchIcon} />
       <input type='search' placeholder='키워드를 입력해주세요' value={valueInput} onChange={handleInputValueChange} />
+      <ul>
+        {searchKeyList?.map((searchedKeys, i) => {
+          const searchedKey = `searchedKey-${i}`
+          return <li key={searchedKey}>{searchedKeys}</li>
+        })}
+      </ul>
     </form>
   )
 }
 
 export default SearchBar
+
+// searchBar 클릭하면 placeholder 사라지기
--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -1,6 +1,18 @@
 import { atom } from 'recoil'
+import { recoilPersist } from 'recoil-persist'
+
+const { persistAtom } = recoilPersist({
+  key: 'storeDatas',
+  storage: localStorage,
+})
 
 export const searchKeyAtom = atom<string>({
   key: 'searchKey',
   default: '',
+})
+
+export const searchKeyListAtom = atom<string[] | undefined>({
+  key: 'searchKeyList',
+  default: [],
+  effects_UNSTABLE: [persistAtom],
 })
