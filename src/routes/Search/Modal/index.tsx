f710e035a486fac1c696f93bcf296c716cb2e4d3
Han Byul <yahb9@naver.com>
2022-06-24 06:15:26 +0900

    Feat: Install react-slick library and make img slider

--- src/routes/Search/Modal/index.tsx
+++ src/routes/Search/Modal/index.tsx
@@ -1,7 +1,10 @@
 import { Dispatch, SetStateAction } from 'react'
+import Slider from 'react-slick'
 
 import { XIcon } from 'assets/svgs'
 
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 import styles from './modal.module.scss'
 
 interface IModalProps {
@@ -14,16 +17,26 @@ const Modal = ({ clickedImges, setOpenModal }: IModalProps) => {
     setOpenModal(false)
   }
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  }
+
   return (
     <div className={styles.background}>
       <button type='button' onClick={handleCloseModalClick}>
         <XIcon className={styles.xIcon} />
       </button>
-      <div className={styles.container}>
-        {clickedImges?.map((img, i) => {
-          const imgKey = `img-${i}`
-          return <img key={imgKey} src={img} alt='모달이미지' />
-        })}
+      <div className={styles.modalContainer}>
+        <Slider {...settings}>
+          {clickedImges?.map((img, i) => {
+            const imgKey = `img-${i}`
+            return <img key={imgKey} className={styles.modalImg} src={img} alt='모달이미지' />
+          })}
+        </Slider>
       </div>
     </div>
   )
